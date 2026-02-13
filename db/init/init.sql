9a36a2fed4738f3ff1552491c23bb1a2435c378a
Alexander Malikov <malikoff.at@gmail.com>
2026-02-13 23:10:56 +0300

    004. Добавлены зависимости полей с users

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     table_id int,
     create_time DATE,
     name VARCHAR(255),
@@ -8,12 +8,14 @@ CREATE TABLE users (
 )
 
 CREATE TABLE time (
-    id int,
+    user_id INT,
     marked DATE
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
 );
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    leader int
+    leader_id INT,
+    FOREIGN KEY (leader_id) REFERENCES users(user_id)
 )
