834386c812dc333cd4c6f893e5781381e8b599c1
Alexander Malikov <malikoff.at@gmail.com>
2026-02-14 18:07:23 +0300

    006. Добавление аттрибутов полей бд

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,21 +1,23 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    table_id int,
-    create_time DATE,
-    name VARCHAR(255),
-    surname VARCHAR(255),
+    table_id INT UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
     patronomic VARCHAR(255)
 );
 
-CREATE TABLE time (
+CREATE TABLE worktime (
     user_id INT,
-    marked DATE,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    day DATE NOT NULL DEFAULT CURRENT_DATE,
+    arrived TIMESTAMPTZ,
+    departed TIMESTAMPTZ
 );
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
+    department_id INT NOT NULL,
     name VARCHAR(255),
-    leader_id INT,
+    leader_id INT NOT NULL,
     FOREIGN KEY (leader_id) REFERENCES users(user_id)
 );
