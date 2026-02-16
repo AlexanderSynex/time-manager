b9e0c94a94a0bfac5c35dbbfbae17a45197f99a9
Alexander Malikov <malikoff.at@gmail.com>
2026-02-16 22:46:57 +0300

    011. Исправление скрипта переконфигурации

--- tools/configure.sh
+++ tools/configure.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+TARGET_BUILD_SYSTEM="Unix Makefiles"
+
+if command -v ninja >/dev/null 2>&1; then
+    TARGET_BUILD_SYSTEM="Ninja";
+fi
+
+echo "Configuring with ${TARGET_BUILD_SYSTEM} at $(pwd)"
+
 mkdir -p build && \
-cd build && \
-conan install . --output-folder=build --build=missing && \
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+conan install . --output-folder=build --build=missing -c tools.cmake.cmaketoolchain:generator="${TARGET_BUILD_SYSTEM}" && \
+cmake --preset conan-release
