94af0a0bf7daa68c31592ccac648d0cd32e532c5
Alexander Malikov <malikoff.at@gmail.com>
2026-03-01 21:49:46 +0300

    040. SQL, Добавлен внутренний номер отдела + проверки на данные

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,26 +1,32 @@
+
+-- Таблицы данных
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    table_id INT UNIQUE NOT NULL,
+    table_id INT UNIQUE NOT NULL CHECK (table_id > 0),
     name VARCHAR(255) NOT NULL CHECK (name != ''),
     surname VARCHAR(255) NOT NULL CHECK (surname != ''),
     patronymic VARCHAR(255)
 );
 
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL CHECK (name != ''),
+    leader_id INT NOT NULL,
+    internal_id INT UNIQUE CHECK (internal_id > 0),
+    FOREIGN KEY (leader_id) REFERENCES users(id)
+);
+
+-- Рабочее время
 CREATE TABLE worktime (
     user_id INT NOT NUlL,
     FOREIGN KEY (user_id) REFERENCES users(id),
     day DATE NOT NULL DEFAULT CURRENT_DATE,
     arrived TIMESTAMPTZ NOT NULL,
     departed TIMESTAMPTZ
+    CONSTRAINT time_check CHECK (departed > arrived)
 );
 
-CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    leader_id INT NOT NULL,
-    FOREIGN KEY (leader_id) REFERENCES users(id)
-);
-
+-- Таблицы отношений
 CREATE TABLE department_workers (
     id SERIAL PRIMARY KEY,
     department_id INT NOT NULL,
