86afcd441b66132327feabd5b00aea4115f1f9af
Alexander Malikov <malikoff.at@gmail.com>
2026-03-22 20:28:26 +0300

    049. Регистрация admin пользователя

--- db/init/01-auth.sql
+++ db/init/01-auth.sql
@@ -3,7 +3,9 @@ CREATE SCHEMA IF NOT EXISTS auth_schema;
 CREATE TABLE IF NOT EXISTS auth_schema.users (
   login TEXT UNIQUE PRIMARY KEY NOT NULL CHECK (TRIM(login) = login),
   table_id BIGINT UNIQUE NOT NULL CHECK (table_id >= 0),
-  password_hash TEXT
+  password_hash TEXT,
+  scopes TEXT[] NOT NULL DEFAULT ARRAY['user'],
+  CONSTRAINT non_empty_scopes CHECK (array_length(scopes, 1) > 0)
 );
 
 CREATE TABLE IF NOT EXISTS auth_schema.tokens (
--- db/init/03-admin-init.sh
+++ db/init/03-admin-init.sh
@@ -2,8 +2,8 @@
 
 psql -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" <<-EOSQL
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-INSERT INTO auth_schema.users(login, table_id, password_hash) 
-VALUES ('admin', 0000, encode(digest('$ADMIN_PASSWORD', 'sha1'), 'hex'))
+INSERT INTO auth_schema.users(login, table_id, password_hash, scopes)
+VALUES ('admin', 0000, encode(digest('$ADMIN_PASSWORD', 'sha1'), 'hex'), ARRAY['admin', 'user'])
 ON CONFLICT (login) DO UPDATE SET
   password_hash = EXCLUDED.password_hash;
 EOSQL
