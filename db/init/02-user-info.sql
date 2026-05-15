b484f8bd887237bdd40d45fd602a9d868462a707
Alexander Malikov <malikoff.at@gmail.com>
2026-05-15 21:33:09 +0300

    Убраны неиспользуемые таблицы

--- db/init/02-user-info.sql
+++ db/init/02-user-info.sql
@@ -1,5 +1,15 @@
 
 CREATE SCHEMA IF NOT EXISTS company_schema;
+
+-- Таблицы данных
+CREATE TABLE IF NOT EXISTS company_schema.users_info (
+    table_id BIGINT PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    patronymic VARCHAR(255),
+    FOREIGN KEY (table_id) REFERENCES auth_schema.users(table_id)
+);
+
 -- Таблицы данных
 CREATE TABLE IF NOT EXISTS company_schema.users_info (
     table_id BIGINT PRIMARY KEY,
@@ -19,29 +29,3 @@ CREATE TABLE IF NOT EXISTS company_schema.worktime (
     CONSTRAINT time_check CHECK (departed > arrived),
     CONSTRAINT unique_worker_date UNIQUE (user_id, day)
 );
-
-
--- CREATE TABLE IF NOT EXISTS department (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     name VARCHAR(255) UNIQUE NOT NULL CHECK (name != ''),
---     leader_id INT NOT NULL,
---     internal_id INT UNIQUE CHECK (internal_id > 0),
---     FOREIGN KEY (leader_id) REFERENCES users(table_id)
--- );
-
--- Таблицы отношений
--- CREATE TABLE department_workers (
---     id SERIAL PRIMARY KEY,
---     department_id INT NOT NULL,
---     worker_id INT NOT NULL,
---     FOREIGN KEY (department_id) REFERENCES department(id),
---     FOREIGN KEY (worker_id) REFERENCES users(table_id)
--- );
-
--- CREATE TABLE department_relations (
---     id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     department_id INT NOT NULL,
---     parent_department_id INT NOT NULL,
---     FOREIGN KEY (department_id) REFERENCES department(id),
---     FOREIGN KEY (parent_department_id) REFERENCES department(id)
--- );
